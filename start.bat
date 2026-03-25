f9afb0946b168060394e7b79214091c1967af3f5
yi124773651 <455954739@qq.com>
2026-03-25 17:06:03 +0800

    add gitignore Signed-off-by: yi124773651 <455954739@qq.com>

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 title AI Daily Digest Server
 echo Starting AI Daily Digest Server...
 echo.
-cd /d D:\open-source-project\ai-daily-digest(web)
+cd D:\open-source-project\DeepDigest
 bun server.ts
 pause
